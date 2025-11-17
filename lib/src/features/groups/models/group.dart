eaf8001c39d4fd1d5ea1c7e2a082390a1854b1f3
RubenLopesDaSilva <ruben.lopesdasilva@ceff.ch>
2025-11-17 14:08:41 +0100

    Entities + Services

---
+++ lib/src/features/groups/models/group.dart
@@ -0,0 +1,13 @@
+class Group {
+  const Group({
+    required this.id,
+    required this.name,
+    required this.words,
+    required this.userId,
+  });
+
+  final String id;
+  final String name;
+  final List<String> words;
+  final String userId;
+}
---
+++ lib/src/features/words/models/word.dart
@@ -0,0 +1,7 @@
+class Word {
+  const Word({required this.id, required this.trad, required this.userId});
+
+  final String id;
+  final Map<String, String> trad;
+  final String userId;
+}
