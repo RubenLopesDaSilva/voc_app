3dd7047f2b495f0906a004307383fb2aa7cc05a3
RubenLopesDaSilva <ruben.lopesdasilva@ceff.ch>
2025-11-17 13:48:36 +0100

    Initial Project

---
+++ android/app/src/main/kotlin/com/example/voc_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:voc_app/src/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
