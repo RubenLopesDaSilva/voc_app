91d1019b37cfbfc735056797bc75837948e9e0bd
BurriSimon <simon.burri@ceff.ch>
2025-12-10 13:35:23 +0100

    Update 13:35 10.12.2025 Navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:voc_app/src/features/repetition/presentation/word_screen.dart';
 import 'package:voc_app/src/common/theme/theme.dart';
+import 'package:voc_app/src/navigation/navigation.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Vocabulary App',
-        theme: lightTheme,
-        home: const WordScreen(groupId: '6926cae85ff4c7ed2aacf390'),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Vocabulary App',
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: lightTheme,
+      routerConfig: goRouter,
     );
   }
 }
---
+++ lib/src/features/repetition/presentation/repetitions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RepetitionsScreen extends StatefulWidget {
+  const RepetitionsScreen({super.key});
+
+  @override
+  State<RepetitionsScreen> createState() => _RepetitionsScreenState();
+}
+
+class _RepetitionsScreenState extends State<RepetitionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Column(children: []));
+  }
+}
